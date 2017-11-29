a3cb26d0bb70c7f9d4ab8939189a6105e410c76d
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-11-29 20:33:45 +0000

    Wed Nov 29 20:33:45 GMT 2017

--- src/main/java/befaster/solutions/FizzBuzz.java
+++ src/main/java/befaster/solutions/FizzBuzz.java
@@ -5,11 +5,13 @@ public class FizzBuzz {
 
     public static String fizzBuzz(Integer number) {
     	String str = String.valueOf(number);
+    	boolean fizzBuzz = false;
     		if(number%15==0 || (number%10==5 && number%10==3)) {
     			str= "fizz buzz";
-    		}else if(number%5==0 || number%10==5) {
+    			fizzBuzz = true;
+    		}else if((number%5==0 || number%10==5) && !fizzBuzz) {
     			str = "buzz";
-    		}else if(number%3==0 || number%10==3) {
+    		}else if((number%3==0 || number%10==3) && !fizzBuzz) {
     			str = "fizz";
     		}
     		
