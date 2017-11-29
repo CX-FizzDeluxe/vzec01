f60e7cbe3e55e2f200b55682c5de44edb581d8a4
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-11-29 20:46:50 +0000

    Wed Nov 29 20:46:50 GMT 2017

--- src/main/java/befaster/solutions/FizzBuzz.java
+++ src/main/java/befaster/solutions/FizzBuzz.java
@@ -5,17 +5,19 @@ public class FizzBuzz {
 
     public static String fizzBuzz(Integer number) {
     	String str = String.valueOf(number);
+    Boolean containsBoth = false;
     
-    	
+    if(String.valueOf(number).contains("5") && String.valueOf(number).contains("3")) {
+		str = "fizz buzz";
+		containsBoth = true;
+	}
     		if(number%15==0) {
     			str= "fizz buzz";
     			
-    		}else if((number%5==0 || String.valueOf(number).contains("5"))) {
+    		}else if((number%5==0 || String.valueOf(number).contains("5")) && !containsBoth) {
     			str = "buzz";
-    		}else if((number%3==0 || String.valueOf(number).contains("3"))) {
+    		}else if((number%3==0 || String.valueOf(number).contains("3")) && !containsBoth) {
     			str = "fizz";
-    		}else if(String.valueOf(number).contains("5") && String.valueOf(number).contains("3")) {
-    			str = "fizz buzz";
     		}
     		
     		return str;
