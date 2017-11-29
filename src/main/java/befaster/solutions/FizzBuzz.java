3b54129d0c0fb7478836ed49637c60029338d4ef
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-11-29 20:24:04 +0000

    Wed Nov 29 20:24:04 GMT 2017

--- src/main/java/befaster/solutions/FizzBuzz.java
+++ src/main/java/befaster/solutions/FizzBuzz.java
@@ -4,7 +4,16 @@ package befaster.solutions;
 public class FizzBuzz {
 
     public static String fizzBuzz(Integer number) {
-       
+    	String str = String.valueOf(number);
+    		if(number%15==0) {
+    			str= "fizz buzz";
+    		}else if(number%5==0) {
+    			str = "buzz";
+    		}else if(number%3==0) {
+    			str = "buzz";
+    		}
+    		
+    		return str;
     }
 
 }
