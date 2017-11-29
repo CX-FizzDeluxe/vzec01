4be00a5e0114e0011c1fcc56193473ddd8d4fbd0
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-11-29 20:28:35 +0000

    Wed Nov 29 20:28:35 GMT 2017

--- src/main/java/befaster/solutions/FizzBuzz.java
+++ src/main/java/befaster/solutions/FizzBuzz.java
@@ -7,9 +7,9 @@ public class FizzBuzz {
     	String str = String.valueOf(number);
     		if(number%15==0) {
     			str= "fizz buzz";
-    		}else if(number%5==0) {
+    		}else if(number%5==0 || number%10==5) {
     			str = "buzz";
-    		}else if(number%3==0) {
+    		}else if(number%3==0 || number%10==3) {
     			str = "fizz";
     		}
     		
